aad32c1fcb35bf908d486d5f0e858392334fbb6e
strawberryoars <devincdennis+1@gmail.com>
2023-12-05 20:04:53 -1000

    Cleanup selection sort

--- selection_sort/README.md
+++ selection_sort/README.md
@@ -2,6 +2,12 @@
 
 Selection sort is a sorting algorithm which is not very fast (O(n^2)). Quicksort is faster which only takes O(n log n).
 
+Selection sort works by repeatedly selecting the smallest (or largest) element from the unsorted portion of the list and moving it to the sorted portion of the list.
+
+I implemented selection sort both in-place and out-of-place.  The in-place algorithm only needs O(1) extra space, whereas the out-of-place algorithm requires O(n) extra space since it needs extra space to put elements in as its sorting them.
+
+## Mathematical Analysis
+
 ```
 selection sort is an in-place comparison sorting algorithm. It has an O(n^2) time complexity, which makes it inefficient on large lists, and generally performs worse than the similar insertion sort. Selection sort is noted for its simplicity and has performance advantages over more complicated algorithms in certain situations, particularly where auxiliary memory is limited.
 ```
--- selection_sort/src/main.rs
+++ selection_sort/src/main.rs
@@ -4,37 +4,55 @@ mod tests {
    use super::*;
 
    #[test]
-   fn test_selection_sort() {
+   fn test_out_of_place_selection_sort() {
        let mut array = vec![1, 3, 5, 7, 9];
-       assert_eq!(selection_sort(&mut array), [1, 3, 5, 7, 9].to_vec());
+       assert_eq!(out_of_place_selection_sort(&mut array), [1, 3, 5, 7, 9].to_vec());
+   }
+
+   #[test]
+   fn test_in_place_selection_sort() {
+       let mut array = vec![1, 3, 5, 7, 9];
+       in_place_selection_sort(&mut array);
+       assert_eq!(array, [1, 3, 5, 7, 9].to_vec());
    }
 }
 
-fn find_smallest(array: &Vec<i32>) -> usize {
-    let mut smallest = array[0];
-    let mut smallest_index = 0;
-    for (index, item) in array.iter().enumerate() {
-        if item < &smallest {
-            smallest = *item;
-            smallest_index = index;
+fn find_smallest(array: &Vec<i32>, start: Option<usize>) -> usize {
+    let start = start.unwrap_or(0);
+    let mut smallest = array[start];
+    let mut smallest_index = start;
+    for i in (start + 1)..array.len() {
+        if array[i] < smallest {
+            smallest = array[i];
+            smallest_index = i;
         }
     }
-
     smallest_index
 }
 
-fn selection_sort(array: &mut Vec<i32>) -> Vec<i32> {
+fn out_of_place_selection_sort(array: &mut Vec<i32>) -> Vec<i32> {
     let mut sorted = vec![];
     for _ in 0..array.len() {
-        let smallest = find_smallest(&array);
+        let smallest = find_smallest(&array, None);
         sorted.push(array[smallest]);
         array.remove(smallest);
     }
     sorted
 }
 
+fn in_place_selection_sort(array: &mut Vec<i32>) {
+    for i in 0..array.len() {
+        let smallest = find_smallest(&array, Some(i));
+        array.swap(i, smallest);
+    }
+}
+
 fn main() {
     let mut array = vec![9, 7, 5, 3, 1];
-    let result = selection_sort(&mut array);
+    let result = out_of_place_selection_sort(&mut array);
     println!("{:?}", result);
+
+    let mut array = vec![9, 7, 5, 3, 1];
+    in_place_selection_sort(&mut array);
+    println!("{:?}", array);
 }
